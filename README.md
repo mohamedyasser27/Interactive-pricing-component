e42f59c1e3a3b5b4042204b1562a4608dd2c32c4
mohamedyasser27 <my2722000@gmail.com>
2022-11-16 09:19:48 +0200

    Finish the slider
    i used pseudo elements to style the slider among the different browsers
    and i used the resize observer to make sure it doesn't break when resizing
    or on smaller screens

--- README.md
+++ README.md
@@ -22,7 +22,6 @@ This is a solution to the [Interactive pricing component challenge on Frontend M
 
 Users should be able to:
 
-
 - View the optimal layout for the app depending on their device's screen size
 - See hover states for all interactive elements on the page
 - Use the slider and toggle to see prices for different page view numbers
---
+++ index.js
@@ -0,0 +1,36 @@
+const priceRange = document.querySelector("#price-range__input");
+const priceRangeProgress = document.querySelector(".price-range__progress");
+
+priceRange.addEventListener("mouseup", () => {
+  priceRange.blur();
+});
+
+function calculateProgressWidth() {
+  var root = document.querySelector(":root");
+  let priceRangeStyle = window.getComputedStyle(priceRange);
+  let width = priceRangeStyle.getPropertyValue("width");
+  width = width.split("");
+  width.pop();
+  width.pop();
+  width = width.join("");
+  width -= 35;
+  let x = (priceRange.value / 100) * width;
+  console.log(priceRange.value)
+
+  root.style.setProperty("--progressValue", `${x}px`);
+}
+
+priceRange.addEventListener("input", calculateProgressWidth);
+
+const observer = new ResizeObserver(() => {
+  calculateProgressWidth();
+});
+
+observer.observe(document.body);
+
+let togglePeriod = document.querySelector(".toggle");
+let togglePeriodBtn = document.querySelector(".toggleBtn");
+
+togglePeriod.addEventListener("click", function (event) {
+  togglePeriodBtn.classList.toggle("annual");
+});
